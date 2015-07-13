c32af931f9b95dda9b03612636c84f3d9c5f0a71
Ryan McGeary <ryan@mcgeary.org>
2015-07-13 13:27:46 -0600

    Defaulted to ISO8601

--- insert-time.el
+++ insert-time.el
@@ -1,6 +1,6 @@
 ;;; insert-time.el --- insert time and date stamps at point
 
-;; Copyright (C) 2001-2007 Ryan McGeary
+;; Copyright (C) 2001-2015 Ryan McGeary
 ;; Author: Ryan McGeary
 ;; Keywords: time date insert format
 
@@ -26,6 +26,7 @@
 ;;     ; (see `format-time-string' for format)
 ;;     (setq insert-date-format "%Y-%m-%d")
 ;;     (setq insert-time-format "%H:%M:%S")
+;;     (setq insert-date-time-format "%Y-%m-%dT%T%z")
 ;;
 ;;     ; keyboard shortcuts
 ;;     (define-key global-map [(control c)(d)] 'insert-date-time)
@@ -41,6 +42,7 @@
 
 ;;; History:
 ;;
+;; 13-Jul-2015 - iso8601 defaults
 ;; 30-Jan-2007 - defvar -> defcustom
 ;; 25-Mar-2001 - created by
 ;;               Ryan McGeary
@@ -48,12 +50,16 @@
 ;;; Code:
 
 (defcustom insert-date-format "%Y-%m-%d"
-  "*Format for `insert-date' and `insert-date-time' (see
-  `format-time-string' for how to format).")
+  "*Format for `insert-date' (see `format-time-string' for how to
+  format).")
 
-(defcustom insert-time-format "%H:%M:%S %z"
-  "*Format for `insert-time' and `insert-date-time' (see
-  `format-time-string' for how to format).")
+(defcustom insert-time-format "%T%z"
+  "*Format for `insert-time' (see `format-time-string' for how to
+  format).")
+
+(defcustom insert-date-time-format "%Y-%m-%dT%T%z"
+  "*Format for `insert-date-time' (see `format-time-string' for
+  how to format).")
 
 (defun insert-date ()
   "Inserts the current date at point in the format specified by
@@ -72,14 +78,13 @@
 specified by `insert-date-format' and `insert-time-format'.  Uses
 `insert-date' and `insert-time'."
   (interactive "*")
-  (insert-date) (insert " ") (insert-time))
+  (insert (format-time-string insert-date-time-format)))
 
 (defun insert-personal-time-stamp ()
   "Inserts the current time-stamp at point for the current user
-in the formats specified by `insert-date-format' and
-`insert-time-format'.  It inserts a timestamp of
-\"(<`user-login-name'> : <`insert-date-format'>
-<`insert-time-format'>)\" Uses `insert-date-time'."
+in the formats specified by `insert-date-time-format'.  It
+inserts a timestamp of \"(<`user-login-name'> :
+<`insert-date-time-format'>)\" Uses `insert-date-time'."
   (interactive "*")
   (insert "(" user-login-name " : ") (insert-date-time) (insert ")"))
 
